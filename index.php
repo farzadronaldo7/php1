e22fb1da191339be194b6c869dda4eb2846e0149
Farzad Ronaldo <32195032+farzadronaldo7@users.noreply.github.com>
2017-10-20 10:29:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,22 @@
 
  <?php
  
+  $txt1 = "Learn PHP";
+  $txt2 = "at Bournemouth and Poole College";
+  $x = 7;
+  $y = 3;
+  
   $Farzad = "me";
+  
+  print "<h2>" .$txt1. "</h2>";
+  print "study PHP " .$txt2. "<br>";
+  print $x + $y;
+  
+  
   echo $Farzad;
   echo "Hello World!";
  
- echo "Hi there - heck I'm online!";  
+  echo "Hi there - heck I'm online!";  
   
   
   ?>
